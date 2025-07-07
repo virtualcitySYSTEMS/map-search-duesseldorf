f9148b97d8101068dfc5f15ccbe4be38bccd1d04
Patrick Würstle <pwuerstle@vc.systems>
2025-07-07 07:48:03 +0200

    feat: update resultsTitle to include text in default options

--- src/defaultOptions.ts
+++ src/defaultOptions.ts
@@ -7,7 +7,7 @@ export default function getDefaultOptions(): PluginConfig {
       '+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs',
     url: 'https://maps.duesseldorf.de/viewer/php/search/search.php?p=',
     params: 'acdipst,25',
-    resultsTitle: ['2'],
+    resultsTitle: ['text', '2'],
     balloon: {
       balloonTitle: 'text',
       balloonSubtitle: ['2'],
